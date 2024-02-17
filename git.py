0f4c1e4af51e154b439b6f4063d2c6672f77f511
u0_154@localhost <156970348+u0-a154@users.noreply.github.com>
2024-02-17 11:17:43 +0700

    git.py

---
+++ git.py
@@ -0,0 +1,539 @@
+import time
+mask = r'''
+
+=====================================================================================================
+
+>>>Spam-WhatsApp
+
+--> https://github.com/FatihArridho/brutal.git
+
+--> https://github.com/ZynnBoy01/Yospam.git [ERROR : INVALID SYNTAX : ILLEGAL ANNOTATION]
+
+--> https://github.com/ZynnBoy01/Spamcall [ERROR : INVALID SYNTAX : ILLEGAL ANNOTATION]
+
+--> https://github.com/Dra-ID/Premium-Call [Untested]
+
+--> https://github.com/goldeneye121/spam-wa-chat.git
+
+--> https://github.com/TZdev7/spam-wa
+
+=====================================================================================================
+
+>>>WiFi-Hack(Rooted-Device-Only)
+
+--> https://github.com/ShTasrif/wipwn [ROOT-ACCESS-NEEDED/SU-ACCESS-NEEDED]
+
+--> https://github.com/Foyej/Psybd [ROOT-ACCESS-NEEDED/SU-ACCESS-NEEDED]
+
+--> https://github.com/kumpulanremaja/wifi [ROOT-ACCESS-NEEDED/SU-ACCESS-NEEDED]
+
+=====================================================================================================
+
+>>>Phsihing
+
+--> https://github.com/kasroudra/pyphisher
+
+--> https://github.com/thewhiteh4t/seeker [Untested]
+
+--> https://github.com/htr-tech/zphisher.git
+
+--> https://github.com/Ignitetch/AdvPhishing
+
+=====================================================================================================
+
+>>>DDoS
+
+--> https://github.com/Mrforces/DDOS [Untested]
+
+--> https://github.com/jseidl/GoldenEye [Warning Will Crash If You're Using HIGH Threads On Termux]
+
+--> https://github.com/palahsu/DDoS-Ripper.git [Untested]
+
+--> https://github.com/otoraritas/DDOS-SHREDDER-V2
+
+--> https://github.com/otoraritas/DDOS-SHREDDER
+
+--> https://github.com/shekyan/slowhttptest [ROOT-ACCESS-NEEDED/SU-ACCESS-NEEDED]
+
+--> https://github.com/Nambel12/NamDoS
+
+--> https://github.com/Cr0DiX666/DDoSv2
+
+--> https://github.com/Nambel12/ToolDDoS
+
+--> https://github.com/piokto/DDoS_botnet_cnc-panel
+
+--> https://github.com/MatrixTM/MHDDoS
+
+--> https://github.com/Sage1221/BlueStarC2 [ User: sagec2 | Password: sage1221 ]
+
+--> https://github.com/TermuxTalha/Talha--DDOS-XD.git
+
+--> https://github.com/4L13199/LITEDDOS
+
+=====================================================================================================
+
+>>>Themes
+
+--> https://github.com/4NK3R-PRODUCT1ON/T4MPILAN-V5
+
+--> https://github.com/Cabbagec/termux-ohmyzsh.git
+
+=====================================================================================================
+
+>>>Tools(MixUp)
+
+--> https://github.com/fahadsyihab06/Toolsv1
+
+--> https://github.com/fahadsyihab06/toolsv2
+
+--> https://github.com/AmmarrBN/Multi-ToolsV2.git [Untested]
+
+--> https://github.com/fahadsyihab06/Spam
+
+--> https://github.com/fahadsyihab/ngrok [Untested]
+
+--> https://github.com/fahadsyihab06/Proot [Untested]
+
+--> https://github.com/catgpt22/ssh-tools-ngrok.git [Untested]
+
+--> https://github.com/GALIRUS404/TOOLSV3 [Untested]
+
+--> https://github.com/AnonHackerr/toolss.git [Untested]
+
+--> https://github.com/iamunixtz/Termux-Ngrok/ [Untested]
+
+--> https://github.com/incredibleindishell/PHP-web-shells [Untested]
+
+--> https://github.com/ferdiak/Nightmare-tools-ssh.v2.git
+
+--> https://github.com/MHProDev/PyRoxy.git
+
+=====================================================================================================
+
+>>>OSINT(s)
+
+--> https://github.com/rajkumardusad/IP-Tracer.git
+
+--> https://github.com/LezhinPTAD/webosint
+
+--> https://github.com/rohmadhidayah/Raikage.git
+
+--> https://github.com/LezhinPTAD/OSINT
+
+--> https://github.com/ANG13T/SatIntel
+
+--> https://github.com/sherlock-project/sherlock
+
+--> https://github.com/sundowndev/phoneinfoga
+
+--> https://github.com/TermuxHackz/X-osint
+
+=====================================================================================================
+
+>>>ChatGPT(AI)
+
+--> https://github.com/MrSanZz/InsomniaGPT-Prompt [ERROR]
+
+--> https://github.com/BlackHat-Abhi/AsXGpt.git [ERROR]
+
+=====================================================================================================
+
+>>>WhatsApp-Thingy
+
+--> https://github.com/Mrforces/UNBANNED
+
+--> https://github.com/hackerxphantom/WA_CRASHER
+
+=====================================================================================================
+
+>>>Virus(Some-Are-Root-Only)
+
+--> https://github.com/fahadsyihab06/virusv1 [ROOT-ACCESS-NEEDED/SU-ACCESS-NEEDED]
+
+=====================================================================================================
+
+>>>Camera-Hack
+
+--> https://github.com/fahadsyihab06/camerah [Untested]
+
+--> https://github.com/sankethj/z-cam [Untested]
+
+=====================================================================================================
+
+>>>FakeRoot(#)
+
+--> https://github.com/RSPLOIT-ID/Fakeroot [Untested]
+
+=====================================================================================================
+
+>>>Scanner
+
+--> https://github.com/security007/007scanner [Untested]
+
+--> https://github.com/zerobyte-id-bak/Bashter [Untested]
+
+--> https://github.com/moham3driahi/xattacker [+Auto-Exploiter]
+
+=====================================================================================================
+
+>>>Sql
+
+--> https://github.com/sqlmapproject/sqlmap.git
+
+=====================================================================================================
+
+>>>Identified-As-Fake
+
+--> https://github.com/ujjawalsaini3/hulk
+
+--> https://github.com/rickyfazaa/MySPAMBot-OTP
+
+--> https://github.com/justahackers/perusak
+
+=====================================================================================================
+>>>Dangerous
+
+                                    [USE IT AT YOUR OWN RISK!]
+
+
+|!NOTE!: THESE SCRIPTS ARE KNOWN TO GIVE DANGEROUS THINGS, SUCH AS VIRUS, MALWARE, RANSOMEWARE, AND |
+|        SOMETHING (WE DON'T KNOW)  THAT IS DANGEROUS. WE RECOMMEND YOU TO NOT TO USE THESE SCRIPT, |
+|        EXCEPT YOU USE IT ON SAFE DEVICE, OR WITH A VIRUS PROTECTION. AND, IF YOU ACCIDENTALLY GET |
+|        A VIRUS WITH THESE SCRIPTS, WE DO NOT RESPONSIBLE TO ANY DAMAGE THE SCRIPT CAUSES. IF YOU  |
+|        WANT TO CHECK FOR A VIRUS PROTECTION, WE RECOMMEND YOU TO SEARCH IT ON ANY TRUSTED WEBS.   |
+|        BEFORE YOU START THE SCRIPT, WE WILL REPEAT AGAIN, WE DO NOT RESPONSIBLE TO ANY DAMAGE THE |
+|        SCRIPT CAUSES.                                                                             |
+
+
+>!>VIRUS-CREATOR
+
+#!> https://github.com/Hacker2425/Ransomware-Builder
+
+
+=====================================================================================================
+>>>Randoms(Unknowns&Unused)
+
+|                        !!![NOTE]!!!                      |
+|Some Of The Scripts Are Fake! Be Aware Of Python2 Scripts!|
+
+--#CameraHack
+
+-> https://github.com/XPH4N70M/HACK-CAMERA.git
+
+-> https://github.com/OnlineHacKing/CameraHack
+
+-> https://github.com/noob-hackers/grabcam
+
+--#Cracker
+
+-> https://github.com/FranataSTORE/HACK-FB
+
+-> https://github.com/LuciverXploit/DarkFb
+
+-> https://github.com/Bitwise-01/Instagram-
+
+-> https://github.com/evildevill/instahack
+
+-> https://github.com/noob-hackers/ighack
+
+-> https://github.com/samsesh/SocialBox-Termux.git
+
+-> https://github.com/anonymousproo/FB-CR4CKER-PRO.git
+
+-> https://github.com/Dra-ID/Free-Instagram
+
+-> https://github.com/exploit-inters/crackrdp
+
+-> https://github.com/sidiqbrewstreet/Crack-ig
+
+-> https://github.com/MrHacker-X/CrackerX.git/
+
+-> https://github.com/Leo-Xd1/INSTA
+
+--#Phishing
+
+-> https://github.com/DarkSecDevelopers/HiddenEye
+
+-> https://github.com/thelinuxchoice/blackeye
+
+-> https://github.com/thelinuxchoice/shellphish
+
+-> https://github.com/Ignitetch/AdvPhishing
+
+-> https://github.com/Ignitetch/Paytm-Phishing
+
+-> https://github.com/Ignitetch/UberEats-Phishing
+
+-> https://github.com/Ignitetch/whatsapp-phishing
+
+-> https://github.com/Ignitetch/Zomato-Phishing
+
+-> https://github.com/Ignitetch/Hotstar-otp-bypass
+
+-> https://github.com/Ignitetch/ola-otpbypass
+
+-> https://github.com/Ignitetch/Amazon-payment-gateway-phishing
+
+--#Virus
+
+-> https://github.com/novalattasya/6VIRUS
+
+--#DDoS
+
+-> https://github.com/V3n0m1997/DDOS-SCRIPT
+
+-> https://github.com/santatic/web2attack
+
+-> https://github.com/WH1T3-E4GL3/eagle-dos.git
+
+-> https://github.com/HyukIsBack/KARMA-DDoS.git
+
+-> https://github.com/teamcyber-glitch/DDOS-V3
+
+--#R.A.T
+
+-> https://github.com/AhMyth/AhMyth-Android-RAT.git
+
+--#Deface
+
+-> https://github.com/yingcrackerhades/pepesGen
+
+-> https://github.com/xPloits3c/Anonsec
+
+-> https://github.com/dr-iman/Drupal-Hunter
+
+-> https://github.com/indoxploit-coders/opencart-bruteforce
+
+-> https://github.com/iamhex/WHMCS-Killer-v4
+
+--#WiFi-Hack
+
+-> https://github.com/derv82/wifite.git
+
+--#Spam
+
+-> https://github.com/Galirus404/spam
+
+--#ShellFinder
+
+-> https://github.com/surabayaxploit/shellfinder/blob/main/shellfinder.txt
+
+--#Dork
+
+-> https://github.com/dr-iman/Wordpress-detector
+
+--#Shell
+
+-> https://github.com/phpshellxyz/webshell
+
+-> https://github.com/AnonRoz-Team/wp_auto_upshell
+
+-> https://github.com/kyo1337/Shell-Finder
+
+--#Exploit
+
+-> https://github.com/security007/laravelExploit
+
+-> https://github.com/anhaxteam/laravel-config-exploit
+
+-> https://github.com/vsec7/Laravel-PhpUnit-Rce-And-Get-Env-Exploiter
+
+-> https://github.com/zerobyte-id-bak/LaravelENV
+
+-> https://github.com/koboi137/darksplitz
+
+-> https://github.com/xpr1m3/sqli-lfi-xss-rce-dorker-and-auto-exploiter-python-
+
+-> https://github.com/shadowz3n/wpexploit
+
+-> https://github.com/kyo1337/revsliderautoexploiter
+
+-> https://github.com/kyo1337/Webdav-Mass-Exploiter
+
+--#Domain
+
+-> https://github.com/zerobyte-id-bak/FinderDomainTakeOver
+
+-> https://github.com/Screetsec/Sudomy
+
+-> https://github.com/rebl0x3r/domain2ip
+
+--#IP-Grabber
+
+-> https://github.com/kyo1337/Mass-IP-Grabbing
+
+--#Vuln.Finder
+
+-> https://github.com/ptonewreckin/cmsdetector
+
+--#CMS-Detector
+
+-> https://github.com/redhathackers/cms-detector
+
+-> https://raw.githubusercontent.com/NinjaCR3/CMS-Detector/master/xcms.py [+Multi-Threads]
+
+--#Penetration-Test
+
+-> https://github.com/exploit-inters/TIDoS-Framework
+
+--#Audio-Fix(KaliNethunter)
+
+-> https://github.com/nikmattullah-github/audiofix-kalinethunterkex
+
+--#OSINT(s)
+
+-> https://github.com/Lucksi/Mr.Holmes
+
+--#Instagram-Thingy
+
+-> https://github.com/htr-tech/unfollow-plus.git
+
+--------------------------------------------Unidentified---------------------------------------------
+
+|                        !!![NOTE]!!!                       |
+|Some Of The Scripts Are Used, But The Use Are Still Unknown|
+
+--> https://github.com/hoaan1995/Cosmic-Mirai
+
+--> https://github.com/DensXyz/denszmbf
+
+--> https://github.com/AmmarrBN/Index-SpamV2
+
+--> https://github.com/TheSpeedX/TBomb.git
+
+--> https://github.com/SPIRAL-0/ANTI-SPIRAL
+
+--> https://github.com/Mudassarch1122/Mudassar
+
+--> https://github.com/BestProfessionals/Muslim-Tech
+
+--> https://github.com/Xiyad69/XIYAD.git
+
+--> https://github.com/gaezvr/Spotify-Premium-Mobile-Full-User-Script
+
+--> https://github.com/BestProfessionals/Muslim-Tech
+
+--> https://github.com/mrking4044/smnew
+
+--> https://github.com/Programmer-Shakib/WPS-WiFi-Hacking-Android-Root_Termux_Installer
+
+--> https://github.com/mrprogrammer2938/Black-Tool-Termux
+
+--> https://github.com/ShTasrif/wipwn [ROOT-ACCESS-NEEDED/SU-ACCESS-NEEDED]
+
+--> https://github.com/Foyej/Psybd [ROOT-ACCESS-NEEDED/SU-ACCESS-NEEDED]
+
+--> https://github.com/OnlineHacKing/WordlistPasswordGenerator
+
+--> https://github.com/dr-iman/cms-detector
+
+--> https://github.com/backdoorhub/shell-backdoor-list/tree/master/shell/php
+
+--> https://github.com/BuddhaLabs/PacketStorm-Exploits
+
+--> https://github.com/alexantr/filemanager
+
+--> https://github.com/0xSearches/sandcastle/
+
+--> https://github.com/noolep/AWCREC
+
+--> https://github.com/noolep/Twilio_Check
+
+--> https://github.com/dr-iman/drupal-hunter
+
+--> https://github.com/Gameye98/Lazymux
+
+--> https://github.com/Dra-ID/Prank-SSC
+
+--> https://github.com/prateek147/DVIA-v2
+
+--> https://github.com/Dra-ID/Prank-Call
+
+--> https://github.com/Galirus404/HACKGG
+
+--> https://github.com/geovedi/indonesian-wordlist
+
+--> https://github.com/MrSanZz/pandora
+
+--> https://github.com/Kedjaw3n/Psqli
+
+--> https://github.com/grafov/hulk
+
+--> https://github.com/Hyperclaw79/HULK-v3
+
+--> https://github.com/Tuhinshubhra/RED_HAWK
+
+--> https://github.com/chaitin/xray/
+
+=====================================================================================================
+
+
+| /GoldenEye-Scripts/ |
+|Actually,  My Teacher|
+
+--> https://github.com/goldeneye121/spam-wa-chat.git [Spam-WhatsApp]
+
+
+=====================================================================================================
+
+                                 [Scripts List Coded By u0_a154]
+
+=====================================================================================================
+
+    Who The Fuck Needs Rules? This Is Our Device, Why They Can Control Us? If They Can, WE Can =)
+
+    =====================================================================================================
+
+ Note:  The Creator Didn't Care Of The Use Of This Tools List, Use It Whatever You Want. The Creator
+        Is Not Responsible Of The Use Of This Tools List
+
+=====================================================================================================
+
+                               Shout Out To These Script Creators!
+                               (And So,  Follow Them If You Want.)
+
+   -FatihArridho    -ShTasrif           -Mrforces           -Cabbagec          -AnonHackerr
+   -ZynnBoy01       -kumpulanremaja     -jseidl             -fahadsyihab06     -iamunixtz
+   -Dra-ID          -kasroudra          -palahsu            -AmmarrBN          -incredibleindishell
+   -goldeneye121    -thewhiteh4t        -otoraritas         -fahadsyihab       -ferdiak
+   -TZdev7          -htr-tech           -shekyan            -catgpt22          -rajkumardusad
+   -Foyej           -Ignitetch          -4NK3R-PRODUCT1ON   -GALIRUS404        -LezhinPTAD
+   -rohmadhidayah   -MrSanZz            -BlackHat-Abhi      -hackerxphantom    -sankethj
+   -RSPLOIT-ID      -security007        -zerobyte-id-bak    -moham3driahi      -ujjawalsaini3
+   -rickyfazaa      -justahackers       -XPH4N70M           -noob-hackers      -FranataSTORE
+   -LuciverXploit   -Bitwise-01         -evildevill         -samsesh           -anonymousproo
+   -exploit-inters  -sidiqbrewstreet    -MrHacker-X         -thelinuxchoice    -DarkSecDevelopers
+   -novalattasya    -V3n0m1997          -santatic           -WH1T3-E4GL3       -HyukIsBack
+   -AhMyth          -teamcyber-glitch   -yingcrackerhades   -xPloits3c         -dr-iman
+   -iamhex          -indoxploit-coders  -derv82             -Galirus404        -surabayaxploit
+   -phpshellxyz     -AnonRoz-Team       -kyo1337            -ptonewreckin      -redhathackers
+   -NinjaCR3        -Lucksi             -hoaan1995          -DensXyz           -nikmattullah-github
+   -TheSpeedX       -SPIRAL-0           -Mudassarch1122     -BestProfessionals -mrking4044
+   -ShTasrif        -mrprogrammer2938   -Programmer-Shakib  -OnlineHacKing     -backdoorhub
+   -BuddhaLabs      -alexantr           -0xSearches         -noolep            -Gameye98
+   -prateek147      -geovedi            -sqlmapproject      -Nambel12          -Cr0DiX666
+   -grafov          -Kedjaw3n           -ANG13T             -MHProDev          -piokto
+   -MatrixTM        -sherlock-project   -sundowndev         -Hyperclaw79       -Tuhinshubhra
+   -chaitin         -Hacker2425         -TermuxHackz        -Sage1221          -TermuxTalha
+   -4L13199
+
+=====================================================================================================
+
+                                      For Script List Update?
+                                            Contact Me!
+                                        +62 895-7097-53090
+                                         [WhatsApp  Only]
+                               Or Simply By Just Reinstall This Tool
+                               https://github.com/u0-a154/succotash 
+
+=====================================================================================================
+2024-02-14                                                                        All Rights Reserved
+=====================================================================================================
+
+'''
+
+lines = mask.strip().split('\n')
+for line in lines:
+    print(line)
+    time.sleep(0.01)  # Adjust the delay (in seconds) to control the speed
